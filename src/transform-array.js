72a8df9de9699ed2418618d6973607c020e6466a
Nikita1814 <kravchenko1814@gmail.com>
2021-09-10 13:24:44 +0300

    add transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,32 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+if (!Array.isArray(arr)){
+  throw "\'arr\' parameter must be an instance of the Array!"
+} 
+let nuArr = arr.map((e) => e = e)
+
+for (let i = 0; i< nuArr.length; i++){
+if (nuArr[i] === '--double-next' && typeof nuArr[i + 1] === 'number' ){
+  nuArr[i] = nuArr[i + 1]
+}else if (nuArr[i] === '--double-prev' && typeof nuArr[i - 1] === 'number'){
+  nuArr[i] = nuArr[i - 1]
+}else if( nuArr[i] === '--discard-prev' && typeof nuArr[i -1] === 'number'){
+  nuArr.splice(i-1,1)
+} else if(nuArr[i] === '--discard-next' && typeof nuArr[i + 1] === 'number'){
+  nuArr.splice(i+1,1)
 }
+}
+
+return nuArr.filter((e) => {
+if (e !=='--discard-next' &&
+e !== '--discard-prev' && 
+e !=='--double-prev'  &&
+e !=='--double-next'){
+return true 
+} else {return false}
+})
+
+}
+ 
