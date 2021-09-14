791fa7e159e64facdd3633acda96a0b662c9ba7a
Nikita1814 <kravchenko1814@gmail.com>
2021-09-14 21:04:25 +0300

    feat: add email-domain function

--- src/st-get-email-domain.js
+++ src/st-get-email-domain.js
@@ -10,7 +10,8 @@ import { NotImplementedError } from '../extensions/index.js';
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-export default function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getEmailDomain(email) {
+  return email.match(/@[^.]+[A-Za-z0-9]*\.\D*/)[0].slice(1)
 }
+
+
